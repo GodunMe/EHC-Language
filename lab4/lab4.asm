b92e6560d956d580d6175ebccf03f1baeaca6249
GodunMe <duonghd1112@gmail.com>
2023-12-24 09:49:04 +0700

    Test

---
+++ lab4/lab4.asm
@@ -0,0 +1,68 @@
+section .data
+    prompt db "Input: ", 0
+    len_prompt equ $ - prompt
+    output1 db "Sum of "
+    len_output1 equ $ - output1
+    output2 db " sequences is: "
+    len_output2 equ $ - output2
+    
+section .bss
+    n resd 1
+    sum resd 1
+
+section .text
+    global _start
+
+_start:
+    ; Print prompt
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, prompt
+    mov edx, len_prompt
+    int 0x80
+
+    ; Read input number
+    mov eax, 3
+    mov ebx, 0
+    mov ecx, n
+    mov edx, 4
+    int 0x80
+
+    dec eax 
+    mov edx, n
+    mov byte [edx + eax], 0  
+
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, output1
+    mov edx, len_output1
+    int 0x80
+    
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, n
+    mov edx, 1
+    int 0x80
+    
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, output2
+    mov edx, len_output2
+    int 0x80
+    
+    mov ecx, 1  
+
+sum_loop:
+    add sum, ecx  
+    inc ecx         
+    cmp ecx, n      
+    jbe sum_loop    
+    
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, sum    
+    mov edx, 4      
+    int 0x80
+
+    mov eax, 1
+    int 0x80
